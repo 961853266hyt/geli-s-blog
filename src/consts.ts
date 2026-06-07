0f4346fef91d7a469f14265789f8b7b45199f151
huyiteng <961853266@qq.com>
2026-06-07 15:12:19 +0800

    refactor(consts): change site meta data

--- src/consts.ts
+++ src/consts.ts
@@ -1,5 +1,5 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_TITLE = 'Astro Blog';
-export const SITE_DESCRIPTION = 'Welcome to my website!';
+export const SITE_TITLE = 'CAN U HEAR ME?';
+export const SITE_DESCRIPTION = 'If you can read this, the signal is already relevant.';
