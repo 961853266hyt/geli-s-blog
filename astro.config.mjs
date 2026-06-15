20cd5109a7581c04f12fe8581f8db418189fc56c
huyiteng <961853266@qq.com>
2026-06-15 19:17:11 +0800

    fix: update site and base paths for correct asset linking

--- astro.config.mjs
+++ astro.config.mjs
@@ -9,7 +9,8 @@ import react from '@astrojs/react';
 
 // https://astro.build/config
 export default defineConfig({
-	site: 'https://example.com',
+	site: 'https://961853266hyt.github.io',
+	base: '/geli-s-blog',
 	integrations: [mdx(), sitemap(), react()],
 	vite: {
 		plugins: [tailwindcss()],
